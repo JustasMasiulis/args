9eee151bf8275fd2ec30bbfc57acfcaf70e65498
JustasMasiulis <justas.masiulis@yahoo.com>
2018-05-25 22:56:46 +0300

    main commit

---
+++ include/reorder_args.hpp
@@ -0,0 +1,74 @@
+﻿/*
+ * Copyright 2018 Justas Masiulis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef JM_REORDER_ARGUMENTS_HPP
+#define JM_REORDER_ARGUMENTS_HPP
+
+#include <utility>
+#include <type_traits>
+
+namespace jm {
+
+    namespace detail {
+
+        template<std::size_t TargetIndex, std::size_t CurrentIndex = 0>
+        struct nth_arg {
+            template<class Discard, class... Rest>
+            static decltype(auto) get(Discard&&, Rest&&... rest)
+            {
+                return nth_arg<TargetIndex, CurrentIndex + 1>::get(
+                    std::forward<Rest>(rest)...);
+            }
+        };
+
+        template<std::size_t TargetIndex>
+        struct nth_arg<TargetIndex, TargetIndex> {
+            template<class Target, class... Rest>
+            static decltype(auto) get(Target&& target, Rest&&...)
+            {
+                return std::forward<Target>(target);
+            }
+        };
+
+        template<class Fn, std::size_t... ArgumentIndices>
+        struct argument_reorderer {
+            Fn _fn;
+
+        public:
+            template<class Function>
+            argument_reorderer(Function&& fn) : _fn(std::forward<Function>(fn))
+            {}
+
+            template<class... Args>
+            decltype(auto) operator()(Args&&... args)
+            {
+                return _fn(nth_arg<ArgumentIndices>::get(std::forward<Args>(args)...)...);
+            }
+        };
+
+    } // namespace detail
+
+
+    template<std::size_t... ArgumentIndices, class Fn>
+    inline detail::argument_reorderer<std::decay_t<Fn>, ArgumentIndices...>
+    reorder_args(Fn&& function)
+    {
+        return { std::forward<Fn>(function) };
+    }
+
+} // namespace jm
+
+#endif
---
+++ test/reorder_args.cpp
@@ -0,0 +1,32 @@
+﻿// reorder_args.cpp : Defines the entry point for the application.
+//
+
+#include "../include/reorder_args.hpp"
+#include <iostream>
+
+void fn(int a, float b, void* c) { std::cout << a << b << c; }
+auto fn2 = [](int a, float b, void* c) { std::cout << a << b << c; };
+auto fn3 = [ fn = fn2, ofn = fn ](auto&&... args) { return (fn(args...), ofn(args...)); };
+
+
+int main()
+{
+    auto reordered1 = jm::reorder_args<2, 1, 0>(fn);
+    // should be the size of a function pointer
+    static_assert(sizeof(reordered1) == sizeof(void*), "invalid size");
+    reordered1(nullptr, 2.f, 1);
+
+
+    auto reordered2 = jm::reorder_args<2, 1, 0>(fn2);
+    std::cout << '\n' << sizeof(reordered2) << ' ' << sizeof(fn2) << '\n';
+    static_assert(sizeof(reordered2) == sizeof(fn2), "invalid size");
+    reordered2(nullptr, 2.f, 1);
+
+    auto reordered3 = jm::reorder_args<2, 1, 0>(fn3);
+    static_assert(sizeof(reordered3) == sizeof(fn3), "invalid size");
+    reordered3(nullptr, 2.f, 1);
+
+	auto reordered4 = jm::reorder_args<2, 1, 0>(std::ref(fn3));
+    static_assert(sizeof(reordered4) == sizeof(void*), "invalid size");
+    reordered4(nullptr, 2.f, 1);
+}
